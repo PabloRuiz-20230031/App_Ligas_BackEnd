2acd172038c84424f0654e21763fcaa3e8be08e4
Pablo Ruiz <20230031@uthh.edu.mx>
2025-08-14 23:51:02 -0600

    Actualización de credenciales, Mongo Uri y servidor, actualizado a Vercel.

---
+++ api/index.js
@@ -0,0 +1,43 @@
+// api/index.js
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const connectDB = require('../config/db');
+require('dotenv').config();
+
+// Conectar DB
+connectDB();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+// Rutas
+app.use('/api/ligas', require('../routes/ligaRoute'));
+app.use('/api/categorias', require('../routes/categoriaRoute'));
+app.use('/api/equipos', require('../routes/equipoRoute'));
+app.use('/api/jugadores', require('../routes/jugadorRoute'));
+app.use('/api/representantes', require('../routes/representanteRoute'));
+app.use('/api/usuarios', require('../routes/usuarioRoute'));
+app.use('/api/cedulas', require('../routes/cedulaRoute'));
+app.use('/api/jornadas', require('../routes/jornadaRoute'));
+app.use('/api/tabla', require('../routes/tablaRoute'));
+app.use('/api/redes-sociales', require('../routes/redsocialRoute'));
+app.use('/api/temporadas', require('../routes/temporadaCompletaRoute'));
+app.use('/api/partidos', require('../routes/partidoRoute'));
+app.use('/api/goleo', require('../routes/goleoRoute'));
+app.use('/api/tarjetas', require('../routes/tablaTarjetasRoute'));
+app.use('/api/info', require('../routes/infoEmpresaRoute'));
+
+// Ruta de prueba
+app.get('/', (req, res) => {
+  res.send('✅ API de Ligas de Fútbol funcionando en Vercel');
+});
+
+// Exportar handler para Vercel
+module.exports = (req, res) => {
+  app(req, res);
+};
